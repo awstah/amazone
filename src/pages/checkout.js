c3692885cf7212c19bfc7ea957bdab7b081a8b16
awstah <62381031+awstah@users.noreply.github.com>
2021-05-24 08:50:44 -0700

    update
    update checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -18,7 +18,7 @@ function checkout() {
     const stripe = await stripePromise;
 
     //call the backend to create a checkout session
-    const checkoutSession = await axios.post("/api/create-checkout-session", {
+    const checkoutSession = await axios.post("/api/create-checkout-session/", {
       items,
       email: session.user.email,
     });
